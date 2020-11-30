64e51f31743d399a12375bb3b617612f616f2177
binchoo <jbinchoo@gmail.com>
2020-11-30 14:12:29 +0900

    Fix typo

--- settings.py
+++ settings.py
@@ -1,20 +1,22 @@
-#directories
 
+ROOT_DIR = '.'
+OUTPUT_DIR = './output'
 
-#config for gazetteer feature
-GAZETTEER_VOCAB = 'vocab/korean_gazette.txt'
-GAZETTEER_LABEL = 'vocab/gazetteer_label.txt'
+TRAIN_FILE = 'ner_train.txt'
+DEV_FILE = 'ner_dev.txt'
 
-gazetteer_model_config = {
+WORD_VOCAB_FILE = "vocab/word_vocab.txt"
+TAG_VOCAB_FILE = "vocab/tag_vocab.txt"
+
+base_config = {
   "mode": "train",
-  "root_dir"='.',
-  "output_dir"='./output',
-  "train_file":"ner_train.txt",
-  "dev_file": "ner_dev.txt",
-  "word_vocab_file":"vocab/word_vocab.txt",
-  "tag_vocab_file":"vocab/tag_vocab.txt",
+  "root_dir": ROOT_DIR,
+  "train_file": TRAIN_FILE,
+  "dev_file": DEV_FILE,
+  "word_vocab_file": WORD_VOCAB_FILE,
+  "tag_vocab_file": TAG_VOCAB_FILE,
   "trained_model_name":"epoch_{}.pt".format(5),
-  "output_dir_path":output_dir,
+  "output_dir_path":OUTPUT_DIR,
   "word_vocab_size":2160,
   "number_of_tags": 14,
   "hidden_size": 100,
@@ -22,7 +24,18 @@ gazetteer_model_config = {
   "embedding_size":100,
   "max_length": 150,
   "batch_size":64,
-  "epoch":50,
-  "features": ['feature.gazetteer.GazetteFeature'],
-  "ngram":4,
+  "epoch":20,
+  "features": None,
 }
+
+from copy import copy
+######                                 ######
+# Configurations of Gazetteer Feature Model#
+######                               ######
+GAZETTEER_VOCAB = 'vocab/korean_gazette.txt'
+GAZETTEER_LABEL = 'vocab/gazetteer_label.txt'
+gazetteer_model_config = copy(base_config)
+gazetteer_model_config['mode'] = 'train'
+gazetteer_model_config['epoch'] = 50
+gazetteer_model_config['features'] = ['feature.gazetteer.GazetteFeature']
+gazetteer_model_config['ngram'] = 4
