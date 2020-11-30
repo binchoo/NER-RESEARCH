621832a03dc101ba42c206c3dca88fe3632ceaf2
binchoo <jbinchoo@gmail.com>
2020-11-30 19:10:30 +0900

    Make pos feature file.

--- feature/postag.py
+++ feature/postag.py
@@ -1,5 +1,5 @@
 from settings import GAZETTEER_VOCAB, GAZETTEER_LABEL
-from konlpy import tag
+from konlpy.tag import mecab
 import numpy as np
 
 class PosTagFeature:
@@ -13,7 +13,7 @@ class PosTagFeature:
         음절로 띄어쓰기 된 End2End 데이터 셋
         문장을 가젯 특성으로 변환
         입력: 음절로 띄어쓰기 된 문장 스트링
-        출력: (시퀀스 최대 길이, 가젯 레이블 수)
+        출력: (시퀀스 최대 길이, 태그 레이블 수)
         '''
         sentence = line.split()
         if len(sentence) <= max_length:
