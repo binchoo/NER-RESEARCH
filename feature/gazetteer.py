1131656e5b5b1774b00192252d3a2ce095f0f137
binchoo <jbinchoo@gmail.com>
2020-11-30 12:31:52 +0900

    ImplemenAdd ngram gazetteer feature for characterized input

--- feature/gazetteer.py
+++ feature/gazetteer.py
@@ -17,9 +17,11 @@ class GazetteFeature:
         with open(vocab_file, 'r', encoding='utf8') as f:
             for korword, nertag in [line.split('\t') for line in f.readlines()]:
                 korword = korword.strip()
-                nertag = nertag.strip()
+                nertags = nertag.strip().split(',')
+                
                 tagset = vocab.get(korword, set())
-                tagset.add(nertag)
+                for nertag in nertags:
+                    tagset.add(nertag)
                 vocab[korword] = tagset
         return vocab
     
@@ -46,6 +48,10 @@ class GazetteFeature:
         return feature
                 
     def transform_word(self, word):
+        '''
+        입력: 자연어 단어
+        출력: (가젯 레이블 수,)
+        '''
         if word in self.vocab:
             hot = [self.label2idx[nertag] for nertag in self.vocab[word]]
         else:
@@ -55,4 +61,30 @@ class GazetteFeature:
         for idx in hot:
             feature[idx] = 1
         return feature
-   
+    
+    def transfrom_charaterized(self, X, ngram):
+        '''
+        입력: (배치, 시퀀스 최대 길이)
+        출력: (배치, 시퀀스 최대 길이, 가젯 레이블 수)
+        '''
+        constant_length = len(X[0])
+        feature = np.empty((len(X), constant_length, len(self.label2idx)))
+        for i, seq in enumerate(X):
+            assert(constant_length == len(seq))
+            feature[i] = self.transfrom_charaterized_line(seq, ngram)
+        return feature
+    
+    def transfrom_charaterized_line(self, line, ngram):
+        '''
+        입력: 자소로 구분된 시퀀스, (시퀀스 최대 길이)
+        출력: (시퀀스 최대 길이, 가젯 레이블 수)
+        '''
+        line_length = len(line)
+        feature = np.zeros((len(line), len(self.label2idx)))
+        for charpos in range(line_length - ngram + 1):
+            ngram_word = "".join(line[charpos: ngram])
+            ngram_word_feature = self.transform_word(ngram_word)
+            for attach in range(ngram):
+                feature[charpos+attach] += ngram_word_feature
+        return feature
+                    
